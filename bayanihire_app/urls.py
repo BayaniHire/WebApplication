1518ed9ddb60a3a6e7261232c3741d3581cabcd3
Trysha <twisyabenitez@gmail.com>
2025-01-01 18:44:16 +0800

    email function

--- bayanihire_app/urls.py
+++ bayanihire_app/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('users_app.urls')),
     path('admindjango/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 
