b1a7c0ab7fe79e7c2d31750740e0977984392934
Trysha <twisyabenitez@gmail.com>
2025-01-10 20:55:28 +0800

    verify email

--- users_app/views.py
+++ users_app/views.py
@@ -49,7 +49,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.conf import settings
 from django.utils.crypto import get_random_string
-from bayanihire_app.models import AccountInformation, OTPVerification
+from bayanihire_app.models import AccountInformation, OTPVerification, VerificationToken
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
